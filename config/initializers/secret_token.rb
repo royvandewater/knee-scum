92daae8789f3617e1f24f84f2b621a2390aad0c5
Roy van de Water <dev+roy@integrumtech.com>
2013-08-07 14:32:00 -0700

    Add base rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KneeScum::Application.config.secret_key_base = '52f446963c0a9d205d930b039a34aa7e4e78b49e083ebb1fd2a277e4d587fa83b5d28196daa4c1f464f83bbdf87c2c8aa4210d657b94e5eb3eebe91faa5cf175'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KneeScum::Application.config.session_store :cookie_store, key: '_knee-scum_session'
